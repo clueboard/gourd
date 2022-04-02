a5e408c1ca4ed3ac95d12d9154ffca9fbbd3f5df
Zach White <skullydazed@gmail.com>
2022-04-02 23:39:33 +0000

    New release: 0.0.7 -> 0.1.0

--- gourd/script.py
+++ gourd/script.py
@@ -5,7 +5,7 @@ from importlib import import_module
 
 from milc import set_metadata
 
-__VERSION__ = '0.0.7'
+__VERSION__ = '0.1.0'
 
 set_metadata(name='Gourd', version=__VERSION__, author='Clueboard')
 
